6292aec89ed6a4ab70fa4559768b0c15c18c321a
Shuo <samuelzhuang815@gmail.com>
2025-08-29 08:20:36 -0500

    Add chapter2 practice code

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,185 @@
+/* let age=25;
+let year=2024;
+console.log(age, year); */
+
+// log things to the console
+//age=26;
+//console.log(age);
+
+/* const points=100;
+console.log(points);
+
+var score=75;
+console.log(score); */
+
+/* // strings
+console.log('Hello, World!');
+
+let email = 'example@email.com'
+console.log(email);
+
+// string concatenation
+let firstName = 'John';
+let lastName = 'Doe';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+
+let index = email.indexOf('@');
+console.log(index);
+
+// common string methods
+let email2 = 'example@email.com';
+// let result2 = email2.lastIndexOf('m');
+// let result2 = email2.slice(0, 5);
+// let result2 = email2.substr(4, 10);
+// let result2 = email2.replace('e', 'x');
+let result2 = email2.replace('e', 'x').toUpperCase();
+console.log(result2); */
+
+/* let radius = 10;
+const pi = 3.14;
+
+//console.log(radius, pi);
+// math operators +, -, *, /, **, %
+console.log(10 / 2);
+let result = radius % 3;
+result = pi * radius**2;
+console.log(result);
+
+// order of operation - B I D M A S
+result = 5 * (10 - 3)**2;
+console.log(result);
+
+let likes = 10;
+likes = likes + 1;
+likes++;
+likes--;
+likes += 10;
+likes -= 5;
+likes *= 2;
+likes /= 2;
+console.log(likes);
+
+// NaN - not a number
+console.log(7 / 'hello');
+console.log('hello' * 2);
+let result2 = 'the blog has ' + likes + ' likes';
+console.log(result2);
+ */
+
+
+/* // template strings
+const title = 'Best reads of 2025';
+const author = 'John Doe';
+const likes = 30;
+
+// concatenation way
+let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+console.log(result);
+
+// template string way
+let result2 = `The blog called ${title} by ${author} has ${likes} likes`;
+console.log(result2);
+
+// creating html templates
+let html = `
+    <h2>${title}</h2>
+    <p>by ${author}</p>
+    <span>This blog has ${likes} likes</span>
+`;
+console.log(html); */
+
+//let ninjas = ['shaun', 'ryu', 'chun-li'];
+/* console.log(ninjas);
+console.log(ninjas[1]);
+console.log(ninjas.length);
+ninjas[1] = 'ken';
+console.log(ninjas);
+let ages = [20, 25, 30, 35];
+console.log(ages);
+console.log(ages[2]);
+let random = ['shaun', 'crystal', 30, 20];
+console.log(random);
+console.log(random.length);  */
+
+// array methods
+
+//let result = ninjas.join(', ');
+//let result = ninjas.indexOf('chun-li');
+//let result = ninjas.concat(['ken', 'yoshi']);
+/* let result = ninjas.push('ken');
+result = ninjas.pop();
+
+console.log(result); */
+
+/* let age = null;
+
+console.log(age, age + 3, `the age is ${age}`); */
+
+/* // booleans & comparisons
+console.log(true, false, "true", "false");
+// methods can return booleans
+let email = 'example@email.com';
+let names = ['mario', 'luigi', 'toad'];
+
+
+//let result = email.includes('@');
+let result2 = names.includes('bowser');
+console.log(result); */
+
+// comparison operators
+//let age = 25;
+/* console.log(age == 25);
+console.log(age == 30);
+console.log(age != 30);
+console.log(age != 25);
+console.log(age > 20);
+console.log(age < 20);
+console.log(age <= 25);
+console.log(age >= 25); */
+
+/* let name = 'shaun';
+console.log(name == 'shaun');
+console.log(name == 'Shaun');
+console.log(name > 'crystal');
+console.log(name > 'Shaun');
+console.log(name > 'Crystal');  */
+
+/* // loose comparison (different types can still be equal)
+
+console.log(age == 25);
+console.log(age == '25');
+console.log(age != 25);
+console.log(age != '25');
+
+// strict comparison (different types cannot be equal)
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25'); */
+
+// type conversion
+let score = '100';
+/* score = Number(score);
+console.log(score + 1);
+console.log(typeof score); */
+let result = Number('hello');
+/* let result = String(50);
+let result = Boolean(100);
+let result = Boolean(0);
+let result = Boolean('0');
+let result = Boolean('');
+console.log(result); */
+console.log(typeof result);
